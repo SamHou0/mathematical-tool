0401236e56ca4bbc330f4a48d9d110c841acd48b
SamHou2007 <61927273+SamHou2007@users.noreply.github.com>
2020-05-05 14:35:29 +0800

    添加项目文件。

---
+++ 数学工具/数学.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace 数学工具
+{
+    partial class 数学工具
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(数学工具));
+            this.start_button = new System.Windows.Forms.Button();
+            this.background_decomposition = new System.ComponentModel.BackgroundWorker();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.input_box = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.status_indication = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.input_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.AutoSize = true;
+            this.start_button.Location = new System.Drawing.Point(180, 158);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(172, 73);
+            this.start_button.TabIndex = 3;
+            this.start_button.Text = "开始";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // background_decomposition
+            // 
+            this.background_decomposition.WorkerReportsProgress = true;
+            this.background_decomposition.WorkerSupportsCancellation = true;
+            this.background_decomposition.DoWork += new System.ComponentModel.DoWorkEventHandler(this.background_decomposition_DoWork);
+            this.background_decomposition.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.background_decomposition_RunWorkerCompleted);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Location = new System.Drawing.Point(454, 158);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(170, 73);
+            this.cancel_button.TabIndex = 4;
+            this.cancel_button.Text = "取消计算";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // input_box
+            // 
+            this.input_box.Location = new System.Drawing.Point(180, 45);
+            this.input_box.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.input_box.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.input_box.Name = "input_box";
+            this.input_box.Size = new System.Drawing.Size(444, 25);
+            this.input_box.TabIndex = 5;
+            this.input_box.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 311);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(444, 25);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "分解质因数";
+            // 
+            // status_indication
+            // 
+            this.status_indication.AutoSize = true;
+            this.status_indication.Location = new System.Drawing.Point(374, 73);
+            this.status_indication.Name = "status_indication";
+            this.status_indication.Size = new System.Drawing.Size(37, 15);
+            this.status_indication.TabIndex = 8;
+            this.status_indication.Text = "空闲";
+            // 
+            // 数学工具
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(784, 420);
+            this.Controls.Add(this.status_indication);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.input_box);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.start_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(802, 467);
+            this.MinimumSize = new System.Drawing.Size(802, 467);
+            this.Name = "数学工具";
+            this.Text = "数学工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.input_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button start_button;
+        private System.ComponentModel.BackgroundWorker background_decomposition;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.NumericUpDown input_box;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label status_indication;
+    }
+}
+
---
+++ 数学工具/数学.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace 数学工具
+{
+    public partial class 数学工具 : Form
+    {
+        private bool Error = false;
+        public 数学工具()
+        {
+            InitializeComponent();
+
+        }
+        public int a = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cancel_button.Enabled = false;
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            cancel_button.Enabled = true;
+            a=Convert.ToInt32(input_box.Value);
+            start_button.Enabled = false;
+            background_decomposition.RunWorkerAsync();
+            status_indication.Text = "计算中...";
+        }
+
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            background_decomposition.CancelAsync();
+        }
+
+        private void background_decomposition_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            Error = false;
+            int y = 1;
+            List<int> list = new List<int>();
+            
+            while (y <= a)
+            {
+                if(background_decomposition.CancellationPending)
+                {
+                    Error = true;
+                    return;
+                }
+                bool zhi_shu = prime_number_judgment(y);
+                if (zhi_shu == true)
+                {
+                    if (a % y == 0)
+                    {
+                        list.Add(y);
+                        a = a / y;
+                        y = 1;
+                    }
+                    else { y += 1; }
+                }
+                else { y += 1; }
+            }
+            string jieguo = "";
+            foreach (int name in list)
+            {
+                jieguo += name + " ";
+            }
+            e.Result = jieguo;
+        }
+
+        private void background_decomposition_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if(Error==true)
+            {
+                start_button.Enabled = true;
+                cancel_button.Enabled = false;
+                MessageBox.Show("错误！操作已取消！");
+                status_indication.Text = "空闲";
+                return;
+            }
+            textBox1.Text = e.Result.ToString();
+            start_button.Enabled = true;
+            status_indication.Text = "空闲";
+            cancel_button.Enabled = false;
+        }
+        private bool prime_number_judgment(int y)
+        {
+
+            List<int> numbers = new List<int>();
+            bool zhi_shu = true;
+            for (int i = 2; i < y; i++)
+            {
+                numbers.Add(i);
+            }
+            foreach (int number in numbers)
+            {
+                if (y % number != 0)
+                {
+                    zhi_shu = true;
+                }
+                else { zhi_shu = false; break; }
+            }
+
+            if (y == 1)
+            {
+                zhi_shu = false;
+            }
+            return zhi_shu;
+        }
+    }
+}
+
