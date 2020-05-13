2fc58d85b09f45d7007332994b6b2780ef45a604
SamHou2007 <61927273+SamHou2007@users.noreply.github.com>
2020-05-13 20:22:00 +0800

    内测更新

--- 数学工具/数学.cs
+++ 数学工具/数学.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Windows.Forms;
 
 namespace 数学工具
@@ -22,10 +23,31 @@ namespace 数学工具
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            if (!File.Exists(@"C:\Users\public\settings.txt"))
+            {
+                File.AppendAllText(@"C:\Users\public\settings.txt", "true\n");
+                File.AppendAllText(@"C:\Users\public\settings.txt", "true\n");
+            }
+            else
+            {
+                string[] result = File.ReadAllLines(@"C:\Users\public\settings.txt");
+                if (result[0] == "true")
+                    radioButton1.Checked = true;
+                else
+                    radioButton2.Checked = true;
+                if (result[1] == "true")
+                    radioButton3.Checked = true;
+                else
+                    radioButton4.Checked = true;
+            }
             start = true;
             cancel_button.Enabled = false;
-            install_update_button.Enabled = false;
-            check_update.RunWorkerAsync();
+            if (radioButton3.Checked)
+            {
+                start_button.Enabled = false;
+                install_update_button.Enabled = false;
+                check_update.RunWorkerAsync();
+            }
         }
 
         private void start_button_Click(object sender, EventArgs e)
@@ -116,46 +138,62 @@ namespace 数学工具
 
         private void check_update_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
         {
+            
+            string[]result =File.ReadAllLines(@"C:\Users\public\settings.txt");
             WebClient webClient = new WebClient();
             File.Delete(@"C:\Users\Public\Setup.msi");
-            try
+            File.Delete(@"C:\Users\Public\relsase_notes.txt");
+            if (result[0] == "true")
             {
-                webClient.DownloadFile("https://samhou2007.github.io/mathematical-tool/latest_release/version_number.txt", @"C:\Users\Public\version_number.txt");
-            }
-            catch
-            {
-                MessageBox.Show("无网络连接！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-            if (File.ReadAllText(@"C:\Users\Public\version_number.txt") != "2.6.5")//TODO:升级版本后记得修改版本号
-            {
-                DialogResult dialogResult = MessageBox.Show("检测到更新，是否下载并安装？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
-                if (dialogResult == DialogResult.Yes)
+                try
                 {
-                    MessageBox.Show("已开始下载，请稍等。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    try
+                    webClient.DownloadFile("https://samhou2007.github.io/mathematical-tool/check_update_information/version_number.txt", @"C:\Users\Public\version_number.txt");
+                    webClient.DownloadFile("https://samhou2007.github.io/mathematical-tool/check_update_information/relsase_notes.txt", @"C:\Users\Public\relsase_notes.txt");
+                }
+                catch
+                {
+                    MessageBox.Show("无网络连接！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (File.ReadAllText(@"C:\Users\Public\version_number.txt") != "2.7.0")//TODO:升级版本后记得修改版本号
+                {
+                    DialogResult downloadResult = MessageBox.Show("检测到更新，是否下载并安装？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    DialogResult showResult = MessageBox.Show("是否显示更新日志？", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (showResult==DialogResult.Yes)
                     {
-                        webClient.DownloadFile("https://samhou2007.github.io/mathematical-tool/latest_release/Setup.msi", @"C:\Users\Public\Setup.msi");
-                        need_update = true;
+                        if (File.Exists(@"C:\Users\Public\history_record.txt"))
+                            Process.Start("notepad.exe", @"C:\Users\Public\history_record.txt");
+                        else
+                            MessageBox.Show("暂时无法显示日志，文件可能已经被删除。","错误",MessageBoxButtons.OK,MessageBoxIcon.Error);
                     }
-                    catch
+                    if (downloadResult == DialogResult.Yes)
                     {
-                        need_update = false;
-                        MessageBox.Show("下载失败，请检查网络连接并重试！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        MessageBox.Show("已开始下载，请稍等。", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        try
+                        {
+                            webClient.DownloadFile("https://samhou2007.github.io/mathematical-tool/Setup/Release/Setup.msi", @"C:\Users\Public\Setup.msi");
+                            need_update = true;
+                        }
+                        catch
+                        {
+                            need_update = false;
+                            MessageBox.Show("下载失败，请检查网络连接并重试！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
                     }
                 }
-            }
-            else
-            {
-                if (start == false)
+                else
                 {
-                    MessageBox.Show("没有更新！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (start == false)
+                    {
+                        MessageBox.Show("没有更新！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
                 }
             }
         }
 
         private void install_update_button_Click(object sender, EventArgs e)
         {
+            start_button.Enabled = false;
             start = false;
             check_update.RunWorkerAsync();
             install_update_button.Enabled = false;
@@ -163,6 +201,7 @@ namespace 数学工具
 
         private void check_update_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
         {
+            start_button.Enabled = true;
             install_update_button.Enabled = true;
             if (need_update == true)
             {
@@ -199,7 +238,8 @@ namespace 数学工具
 
         private void input_box_ValueChanged(object sender, EventArgs e)
         {
-            if (radioButton1.Checked&&Convert.ToInt32(input_box.Value)<=10000)
+            string[] result = File.ReadAllLines(@"C:\Users\public\settings.txt");
+            if (result[0]=="true"&&Convert.ToInt32(input_box.Value)<=10000)
             {
                 cancel_button.Enabled = true;
                 a = Convert.ToInt32(input_box.Value);
@@ -210,6 +250,24 @@ namespace 数学工具
                     status_indication.Text = "计算中...";
                 }
             }
+        }
+
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            File.Delete(@"C:\Users\public\settings.txt");
+            if (radioButton1.Checked)
+                File.AppendAllText(@"C:\Users\public\settings.txt", "true\n");
+            else
+                File.AppendAllText(@"C:\Users\public\settings.txt","false\n");
+            if (radioButton3.Checked)
+            {
+                File.AppendAllText(@"C:\Users\public\settings.txt", "true\n");
+            }
+            else
+            {
+                File.AppendAllText(@"C:\Users\public\settings.txt", "false\n");
+            }
+            MessageBox.Show("保存成功！", "提示",MessageBoxButtons.OK,MessageBoxIcon.Information);
         }
     }
 }
